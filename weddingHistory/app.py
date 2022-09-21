6b71214ecde4902032e2799e70999327cfbbf3fb
Minhyeonhong <mhh2029@gmail.com>
2022-09-21 12:59:13 +0900

    웨딩 메인

--- weddingHistory/app.py
+++ weddingHistory/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+from bson.objectid import ObjectId
 from pymongo import MongoClient
 client = MongoClient('mongodb+srv://minhyeonhong:m1245415@cluster0.1xfvvjw.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
@@ -10,37 +11,65 @@ db = client.dbsparta
 def home():
     return render_template('index.html')
 
+@app.route("/users", methods=["POST"])
+def get_user():
+    user = list(db.users.find({}, {'_id': False}))
+    return jsonify({'user': user})
 
-@app.route("/bucket", methods=["POST"])
-def bucket_post():
-    bucket_receive = request.form['bucket_give']
+@app.route("/main", methods=["GET"])
+def main_get():
+    try:
+        id = request.args.get('id')  # 'id'라는 key값
 
-    bucket_list = list(db.bucket.find({}, {'_id': False}))
-    count = len(bucket_list)+1
+        check_list = list(db.check_list.find({'id':id}).sort('day'))
+    except Exception as e:
+        print(e)
 
-    doc = {'num':count,'bucket': bucket_receive,'done':0}
-    db.bucket.insert_one(doc)
+    return render_template('main.html', check_list=check_list)
 
-    return jsonify({'msg': '등록 완료!'})
+@app.route("/main/add", methods=["POST"])
+def content_post():
+    try:
+        mywork = request.form['mywork']
+        mydate = request.form['mydate']
+        user = request.form['user']
 
+        doc = {'id': user, 'work': mywork, 'day': mydate, 'done': 0}
+        db.check_list.insert_one(doc)
+        msg = '등록 완료!'
 
-@app.route("/bucket/done", methods=["POST"])
-def bucket_done():
-    num_receive = request.form['num_give']
-    db.bucket.update_one({'num':int(num_receive)},{'$set':{'done':1}})
-    return jsonify({'msg': '버킷 완료!'})
+        # 같은 날짜 못넣게 하기 (노쓸모)
+        # check_list = list(db.check_list.find({'id':user,'day':mydate}, {'_id': False}))
+        # if(len(check_list) == 0):
+        #      db.check_list.insert_one(doc)
+        #      msg = '등록 완료!'
+        # else:
+        #      msg = '같은 날짜의 할 일이 있네요 ㅠㅠ'
+    except Exception as e:
+        print(e)
+        msg = '등록 실패'
 
-@app.route("/bucket/unDone", methods=["POST"])
-def bucket_undone():
-    num_receive = request.form['num_give']
-    db.bucket.update_one({'num':int(num_receive)},{'$set':{'done':0}})
-    return jsonify({'msg': '버킷 취소 완료!'})
+    return jsonify({'msg': msg})
 
+@app.route("/main/modDone", methods=["POST"])
+def modDone():
+    try:
+        userId = request.form['userId']
+        _id = request.form['_id']
+        done = request.form['done']
 
-@app.route("/bucket", methods=["GET"])
-def bucket_get():
-    bucket_list = list(db.bucket.find({}, {'_id': False}))
-    return jsonify({'buckets': bucket_list})
+        if (int(done) == 1):
+            msg = '할 일 완료!'
+        elif (int(done) == 0):
+            msg = '할 일 취소!'
+
+        db.check_list.update_one({'_id': ObjectId(_id),'id':userId}, {'$set': {'done': int(done)}})
+
+    except Exception as e:
+        print(e)
+        msg = '에러'
+
+    return jsonify({'msg': msg})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
